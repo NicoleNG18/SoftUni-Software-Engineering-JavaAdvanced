132331a269139d3ec2b3ba1ad47ebb9e68635fa6
Nikol Georgieva <nicole.georgieva18@abv.bg>
2022-12-22 17:27:30 +0200

    defining classes exercise

---
+++ src/definingClasses/bankAccountTask/BankAccount.java
@@ -0,0 +1,31 @@
+package definingClasses.bankAccountTask;
+
+public class BankAccount {
+    private int id;
+    private double balance;
+    private static double interestRate = 0.02;
+    private static int nextId = 1;
+
+    public BankAccount() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public static void setInterestRate(double interest) {
+        interestRate = interest;
+
+    }
+
+    public double getInterest(int years) {
+        return interestRate * years * this.balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
