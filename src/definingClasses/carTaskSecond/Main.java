05453a973f516eff063ff44eca31637cf772dd12
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-01-02 21:40:49 +0200

    defining classes exercise

---
+++ src/definingClasses/carTaskSecond/Main.java
@@ -0,0 +1,30 @@
+package definingClasses.carTaskSecond;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n=Integer.parseInt(scanner.nextLine());
+
+        List<Car> myCars=new ArrayList<>();
+
+        while(n-->0){
+
+            String [] helpArr=scanner.nextLine().split("\\s+");
+            if(helpArr.length==1){
+                myCars.add(new Car(helpArr[0]));
+
+            } else {
+                myCars.add(new Car(helpArr[0], helpArr[1], Integer.parseInt(helpArr[2])));
+            }
+
+        }
+
+        myCars.forEach(e-> System.out.println(e.carInfo()));
+
+    }
+}
