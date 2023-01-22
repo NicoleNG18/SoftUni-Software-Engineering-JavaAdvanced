ebb3536d67a0c91319f502c089e76adff34b0db9
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-01-22 14:13:02 +0200

    iterators/ strategy pattern

---
+++ src/iterators/strategyPattern/CompareByAge.java
@@ -0,0 +1,11 @@
+package iterators.strategyPattern;
+
+import java.util.Comparator;
+
+public class CompareByAge implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        return Integer.compare(o1.getAge(),o2.getAge());
+    }
+}
+
