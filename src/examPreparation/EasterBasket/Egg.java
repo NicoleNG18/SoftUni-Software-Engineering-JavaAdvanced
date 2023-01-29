4d13b617127bc22739c9211eb8d98352dc58343d
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-01-29 10:23:45 +0200

    advanced / exam preparation

---
+++ src/examPreparation/EasterBasket/Egg.java
@@ -0,0 +1,43 @@
+package examPreparation.EasterBasket;
+
+public class Egg {
+    private String color;
+    private int strength;
+    private String shape;
+
+
+    public Egg(String color, int strength, String shape) {
+        this.color = color;
+        this.strength = strength;
+        this.shape = shape;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s egg, with %d strength and %s shape.",this.getColor(),this.getStrength(),this.getShape());
+    }
+}
