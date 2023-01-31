0f7fed1077722cb7f3ae9f9fc6a64dc6f9260b5b
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-01-31 10:25:20 +0200

    advanced / exam preparation

---
+++ src/examPreparation/hotel/Person.java
@@ -0,0 +1,55 @@
+package examPreparation.hotel;
+
+public class Person {
+    public String name;
+    public int id;
+    public int age;
+    public String hometown = "n/a";
+
+    public Person(String name, int id, int age, String hometown) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.hometown = hometown;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    @Override
+    public String toString() {
+        return "Person " + this.getName() + ": " + this.getId() + ", Age: " + this.getAge() + ", Hometown: " + this.getHometown();
+    }
+
+
+}
+
